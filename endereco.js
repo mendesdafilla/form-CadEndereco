de5c0b78c9d5bc5bdc5c0a198249eca45ba984dd
mendesdafilla <dafila.mendes@alunoescolassociais.org.br>
2024-09-26 08:18:34 -0300

    alterar js

--- endereco.js
+++ endereco.js
@@ -5,14 +5,14 @@
 
 // Limpar consulta de form ja realizada 
 const LimparFormulario = () =>{
-    document.getElementById("Rua").value = "";
+    document.getElementById("logradouro").value = "";
     document.getElementById("bairro").value = '';
-    document.getElementById('cidade').valeu = '';
-    document.getElementById.apply('estado').veleu = '';
+    document.getElementById('localidade').valeu = '';
+    document.getElementById.apply('uf').veleu = '';
 }
 
 //verificar se o Cep e valido
-const eNumero = (numero) => /^[0-9]+$/.test(numero);
+const eNumero = (numero) => /^[0-9]+$/.test(numero);//expresao regular
 //verificar o tamanho do cep
 const cepValido = (cep) => cep.length == 8 && eNumero(cep);
 
@@ -24,3 +24,27 @@ const preencherFormulario = (endereco) =>{
     document.getElementById('localidade').value = endereco.localidade; 
     document.getElementById('uf').value = endereco. uf; 
 }
+
+  // função para consumo de API ViaCEP
+const pesquisarCEP = async() => {
+    LimparFormulario();
+    const url = `http://viacep.com.br/ws/${cep.value}/json/`;
+    if(cepValido(cep.value)){
+        const dados  = await fetch(url);
+        const addres = await dados.json();
+
+        if(addres.hasOwnProperty()){
+             alert('CEP nao encontrado');
+        }else{
+            preencherFormulario(addres);
+
+        }
+        
+    }else{
+      alert('CEP Incorreto');
+    }
+
+}
+
+//Executa a ação de preenchimento do formulario ao deixar o campo de CEP
+ document.getElementById('cep').addEventListener('focusout', pesquisarCE); 
